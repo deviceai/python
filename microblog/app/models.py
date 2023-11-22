7433ff74811d20316cbcdedb883b7c1f1174fff6
Aleksandr <device.ai@gmail.com>
2023-11-22 14:42:41 +0200

    Email support

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,9 +1,12 @@
 from datetime import datetime
+from time import time
+
+import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
-from app import  db
+from app import db, app
 from app import login
 
 @login.user_loader
@@ -62,6 +65,20 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)

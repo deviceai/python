c0fd0b2d174bf994abec8d276c5a4d328defc2d3
Aleksandr <device.ai@gmail.com>
2023-12-06 17:30:59 +0200

    Dates and Times (flask-moment)

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -10,6 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,7 +19,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
-from flask_bootstrap import Bootstrap
+moment = Moment(app)
 
 from app import routes, models, errors
 
